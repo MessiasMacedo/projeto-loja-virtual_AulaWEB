8bb5d587955d0579ebf1bee35a5e1fc9480f68db
Caio Oliveira <caio.oliveira07@gmail.com>
2026-09-08 15:16:38 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import hero from './assets/hero.png'
 import Benefits from './components/Benefits'
 import Categories from './components/Categories'
+import Footer from './components/Footer'
+import Header from './components/Header'
 import HeroSection from './components/HeroSection'
 import Products from './components/Products'
 
 const App = () => {
   return (
     <div className="app">
+      <Header />
       <HeroSection 
         titulo ="Ofertas imperdíveis para você"
         subtitulo = "Até 30% de desconto em produtos selecionados. Aproveite!"
@@ -17,6 +20,7 @@ const App = () => {
       <Categories />
       <Products />
       <Benefits />
+      <Footer />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,15 @@ import App from './App'
 import ProductsPage from './pages/ProductsPage'
 import ContatoPage from './pages/ContatoPage'
 import PageNotFound from './pages/PageNotFound'
-import Layout from './pages/Layout'
-import CategoriesPage from './pages/CategoriesPage'
 
 const router = createBrowserRouter([
   {
-    element: <Layout/>,
-    errorElement: <PageNotFound/>,
-    children: [
-      {index: true, element: <App/>},
-      {path: "produtos", element: <ProductsPage/>},
-      {path: "categorias/:category", element: <CategoriesPage/>},
-      {path: "contato", element: <ContatoPage />}
-    ]
-  }
+    path: "/",
+    element: <App/>,
+    errorElement: <PageNotFound/>
+  },
+  {path: "produtos", element: <ProductsPage/>},
+  {path: "contato", element: <ContatoPage />}
 ]);
 
 createRoot(document.getElementById('root')).render(
